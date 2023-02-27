71536ec5b2816a82397f9c27389e1a49a959b1a8
Alan Steremberg <alan.steremberg@gmail.com>
2023-02-26 16:58:02 -0800

    updated sys, release

--- Arcade-DigDug.sv
+++ Arcade-DigDug.sv
@@ -37,13 +37,14 @@ module emu
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
 	output        VGA_SCALER, // Force VGA scaler
+	output        VGA_DISABLE, // analog out is off
 
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
 
 `ifdef MISTER_FB
-	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
+	// Use framebuffer in DDRAM
 	// FB_FORMAT:
 	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
 	//    [3]   : 0=16bits 565 1=16bits 1555
@@ -162,15 +163,16 @@ assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQM
 assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 
-assign VGA_F1    = 0;
-assign VGA_SCALER= 0;
-assign USER_OUT  = '1;
-assign LED_USER  = ioctl_download;
-assign LED_DISK  = 0;
-assign LED_POWER = 0;
-assign BUTTONS   = 0;
-assign AUDIO_MIX = 0;
-assign HDMI_FREEZE = 0;
+assign VGA_F1     = 0;
+assign VGA_SCALER = 0;
+assign VGA_DISABLE= 0;
+assign USER_OUT   = '1;
+assign LED_USER   = ioctl_download;
+assign LED_DISK   = 0;
+assign LED_POWER  = 0;
+assign BUTTONS    = 0;
+assign AUDIO_MIX  = 0;
+assign HDMI_FREEZE= 0;
 assign FB_FORCE_BLANK = 0;
 
 wire [1:0] ar = status[20:19];
